292f6022f547741ed47be2b2a209239fb5f2bad4
OvisMaximus <OvisMaximus@users.noreply.github.com>
2025-06-20 12:42:13 +0200

    excercise 4.7 - Helper Functions and Unit Tests, step 5

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -18,8 +18,7 @@ const mostBlogs = (blogs) => {
     if( !blogs || blogs.length === 0 ) return null
     const articlesByAuthor = _.groupBy(blogs, blog => blog.author)
     const authors = Object.keys(articlesByAuthor)
-    const mostBlogsAuthor = authors.reduce(
-        (a, b) => articlesByAuthor[a].length > articlesByAuthor[b].length ? a : b)
+    const mostBlogsAuthor = _.maxBy(authors, (author) => articlesByAuthor[author].length)
     return {
         author: mostBlogsAuthor,
         blogs: articlesByAuthor[mostBlogsAuthor].length
@@ -31,8 +30,7 @@ const mostLikes = (blogs) => {
     const articlesByAuthor = _.groupBy(blogs, blog => blog.author)
     const likesOfAuthor = (author) => articlesByAuthor[author].reduce((a, b) => a + b.likes, 0)
     const authors = Object.keys(articlesByAuthor)
-    const mostLikesAuthor = authors.reduce(
-        (a, b) => likesOfAuthor(a) > likesOfAuthor(b) ? a : b)
+    const mostLikesAuthor = _.maxBy(authors, likesOfAuthor)
     return {
         author: mostLikesAuthor,
         likes: likesOfAuthor(mostLikesAuthor)
