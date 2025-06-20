29805ffeba827f4d35a2e8cb10c0c6e0734c4c4d
OvisMaximus <OvisMaximus@users.noreply.github.com>
2025-06-20 16:50:11 +0200

    https://fullstackopen.com/en/part4/testing_the_backend#test-environment

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.DB_URL
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DB_URL
+    : process.env.DB_URL
 
 module.exports = { MONGODB_URI, PORT }
