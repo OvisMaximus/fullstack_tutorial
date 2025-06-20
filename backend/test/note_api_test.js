f0e6b09c121f3254572bd43871acbae4c826d0a2
OvisMaximus <OvisMaximus@users.noreply.github.com>
2025-06-20 17:56:10 +0200

    https://fullstackopen.com/en/part4/testing_the_backend#initializing-the-database-before-tests

--- backend/test/note_api_test.js
+++ backend/test/note_api_test.js
@@ -1,10 +1,30 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
 
 const api = supertest(app)
+
+const initialNotes = [
+    {
+        content: 'HTML is easy',
+        important: false,
+    },
+    {
+        content: 'Browser can execute only JavaScript',
+        important: true,
+    },
+]
+
+beforeEach(async () => {
+    await Note.deleteMany({})
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
     // noinspection JSCheckFunctionSignatures,JSUnresolvedReference
@@ -18,7 +38,7 @@ test('all notes are returned', async () => {
     // noinspection JSCheckFunctionSignatures
     const response = await api.get('/api/notes')
     // noinspection JSUnresolvedReference
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
