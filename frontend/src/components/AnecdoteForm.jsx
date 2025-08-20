803b81a08538565b056c97e47915fb4e930ce4c2
OvisMaximus <OvisMaximus@users.noreply.github.com>
2025-08-20 13:04:18 +0200

    6.15 Anecdotes and the Backend, step 2

--- frontend/src/components/AnecdoteForm.jsx
+++ frontend/src/components/AnecdoteForm.jsx
@@ -1,17 +1,20 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer.js'
 import { successMessage } from './Notification.redux.jsx'
+import anecdoteService from '../services/anecdotesService.js'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
 
     const addAnecdote = (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
-        dispatch(createAnecdote({ content: content, id: anecdotes.length }))
-        successMessage(dispatch, `you added ${content}`)
+        anecdoteService.create({content, important:false}).then(newAnecdote => {
+            dispatch(createAnecdote(newAnecdote))
+            successMessage(dispatch, `you added ${content}`)
+        })
     }
+
 
     return (
         <form onSubmit={addAnecdote}>
